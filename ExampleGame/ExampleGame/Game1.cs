7050168b152b7c9f804d4a230336ceb921e66010
Ronald Bruce <ronald.bruce@wsu.edu>
2019-01-17 17:00:17 -0800

    adding shooting with bullit bills

--- ExampleGame/ExampleGame/Game1.cs
+++ ExampleGame/ExampleGame/Game1.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace ExampleGame
 {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
+
+    class Bullets
+    {
+        public Texture2D texture;
+
+        public Vector2 position;
+        public Vector2 velocity;
+        public Vector2 origin;
+
+        public bool isVisible;
+
+        public Bullets(Texture2D newTexture)
+        {
+            texture = newTexture;
+            isVisible = false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, 0f, origin, 1f, SpriteEffects.None, 1);
+        }
+
+    }
     public class Game1 : Game
     {
         Texture2D ballTexture;
@@ -15,16 +40,17 @@ namespace ExampleGame
 
         Texture2D squareTexture;
         Vector2 squarePosition;
-
+        
         //Key mapping
         Keys upKey = Keys.Up;
         Keys downKey = Keys.Down;
         Keys leftKey = Keys.Left;
         Keys rightKey = Keys.Right;
-
+        Keys spacebar = Keys.Space;
+        KeyboardState pastKey;
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;     
-        
+        SpriteBatch spriteBatch;
+        List<Bullets> bullets = new List<Bullets>();
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -94,14 +120,49 @@ namespace ExampleGame
                 ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (kstate.IsKeyDown(rightKey))
                 ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
+            if(kstate.IsKeyDown(spacebar) && pastKey.IsKeyUp(Keys.Space))
+            {
+                shoot();
+            }
+            pastKey = Keyboard.GetState();
             //----------------v This MathHelper.Min(...) blob is essentially collision detection?
             ballPosition.X = MathHelper.Min(MathHelper.Max(ballTexture.Width / 2, ballPosition.X), graphics.PreferredBackBufferWidth - ballTexture.Width / 2);
             ballPosition.Y = MathHelper.Min(MathHelper.Max(ballTexture.Height / 2, ballPosition.Y), graphics.PreferredBackBufferHeight - ballTexture.Height / 2);
 
             base.Update(gameTime);
+            UpdateBullets();
         }
+        public void shoot()
+        {
+            Bullets newBullet = new Bullets(Content.Load<Texture2D>("bill2"));
+            newBullet.velocity = new Vector2(0, -10);
+            newBullet.position = ballPosition;
+            newBullet.isVisible = true;
 
+            if (bullets.Count < 20)
+                bullets.Add(newBullet);
+        }
+        public void UpdateBullets()
+        {
+            foreach (Bullets bullet in bullets)
+            {
+                bullet.position += bullet.velocity;
+                if (Vector2.Distance(bullet.position, ballPosition) > 600)
+                    bullet.isVisible = false;
+            }
+            for (int i = 0; i < bullets.Count; i++)
+            {
+                if (!bullets[i].isVisible)
+                {
+                    bullets.RemoveAt(i);
+                    i--;
+
+
+                }
+
+
+            }
+        }
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -132,8 +193,10 @@ namespace ExampleGame
                 Vector2.One,
                 SpriteEffects.None,
                 0f);
+            
+            foreach (Bullets bullet in bullets)
+                bullet.Draw(spriteBatch);
             spriteBatch.End();
-
             base.Draw(gameTime);
         }
     }
