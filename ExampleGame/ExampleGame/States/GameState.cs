a646782c25a23b8ef6a0ba8ecc0fbb5a629002df
Arika Arnzen <arikaarnzen11@live.com>
2019-01-28 11:24:04 -0800

    Added a main menu with game states

---
+++ ExampleGame/ExampleGame/States/GameState.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ExampleGame.States
+{
+    public class GameState : State
+
+    {
+        Texture2D backgroundTexture;
+        Player player;
+        Enemy grunt;
+        GraphicsDeviceManager _graphics;
+        public GameState(Game1 game, GraphicsDeviceManager graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            _graphics = graphicsDevice;
+            player = new Player(content);
+            player.Initialize(100f, new Vector2(graphicsDevice.PreferredBackBufferWidth / 2,
+                                       graphicsDevice.PreferredBackBufferHeight / 2));
+
+            grunt = new Enemy(content, new Vector2(1, 0));
+            grunt.Initialize(0f, new Vector2(graphicsDevice.PreferredBackBufferWidth / 3,
+                                         graphicsDevice.PreferredBackBufferHeight / 3));
+
+            player.Load(content.Load<Texture2D>("player"));
+            grunt.Load(content.Load<Texture2D>("invader1"));
+            backgroundTexture = content.Load<Texture2D>("spaceBackground");
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(
+                backgroundTexture,
+                new Rectangle(0, 0, 800, 480),
+                Color.White);
+
+            player.Draw(spriteBatch);
+            grunt.Draw(spriteBatch);
+
+            spriteBatch.End();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            // implement
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var kstate = Keyboard.GetState();
+
+            grunt.Update(gameTime);
+            player.Update(gameTime);
+            player.boundsCheck(_graphics);
+        }
+    }
+}
---
+++ ExampleGame/ExampleGame/States/MenuState.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ExampleGame.States
+{
+    public class MenuState : State
+    {
+        private List<Component> _components;
+        public MenuState(Game1 game, GraphicsDeviceManager graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            var buttonTexturePlay = _content.Load<Texture2D>("Controls/ButtonPlay");
+            var buttonTextureQuit = _content.Load<Texture2D>("Controls/ButtonQuit");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
+            var newGameButton = new Button(buttonTexturePlay, buttonFont)
+            {
+                Position = new Vector2(260, 100),
+                Text = "New Game",
+            };
+
+            newGameButton.Click += NewGameButton_Click;
+
+            var quitGameButton = new Button(buttonTextureQuit, buttonFont)
+            {
+                Position = new Vector2(245, 200),
+                Text = "Quit",
+            };
+
+            quitGameButton.Click += QuitGameButton_Click;
+
+            _components = new List<Component>()
+            {
+                newGameButton,
+                quitGameButton,
+            };
+        }
+
+        private void QuitGameButton_Click(object sender, EventArgs e)
+        {
+            _game.Exit();
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            // load new game state
+            _game.ChangeState(new GameState(_game, _graphicsDevice, _content));
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            var backgroundTexture = _content.Load<Texture2D>("spaceBackground");
+            spriteBatch.Begin();
+            spriteBatch.Draw(
+                backgroundTexture,
+                new Rectangle(0, 0, 800, 480),
+                Color.White);
+            foreach (var component in _components)
+            {
+                component.Draw(gameTime, spriteBatch);
+            }
+            spriteBatch.End();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            // remove sprites if they are unneeded
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _components)
+            {
+                component.Update(gameTime);
+            }
+        }
+    }
+}
